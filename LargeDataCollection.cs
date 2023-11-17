813fa4ba8f868524fe814e8210fe2932131848ef
Rohithkumar <rohithkumargande@gmail.com>
2023-11-17 13:48:16 +0530

    assignment14

---
+++ LargeDataCollection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+       public class LargeDataCollection<T> : IDisposable 
+       {
+           private List<T> internalData;
+
+           public LargeDataCollection(IEnumerable<T> initialData)
+           {
+               internalData = new List<T>(initialData);
+           }
+
+           public void AddElement(T element)
+           {
+               internalData.Add(element);
+     Console.WriteLine($"Added element: {element}");
+           }
+           public void RemoveElement(T element)
+           {
+               internalData.Remove(element);
+    Console.WriteLine($"Removed element: {element}");
+           }
+           public void AccessElements()
+           {
+               foreach (var element in internalData)
+               {
+                   Console.WriteLine(element);
+               }
+           }
+
+           public void Dispose()
+           {
+               internalData = null;
+               GC.SuppressFinalize(this);
+           }
+       } 
+  
+    }
+    
+
+
